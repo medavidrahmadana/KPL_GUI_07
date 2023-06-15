01c6a8bac0d1a8234b4ae1e4a786bce9bcbcda9c
gabrielasheryll <126972165+gabrielasheryll@users.noreply.github.com>
2023-06-15 22:01:49 +0700

    Create Form Update
    Membuat form update sebelum digabung dengan form lainnya :)

---
+++ BarangKamuUpdate/BarangKamuUpdate/FormUpdate.cs
@@ -0,0 +1,25 @@
+namespace BarangKamuUpdate
+{
+    public partial class FormUpdate : Form
+    {
+        public FormUpdate()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
